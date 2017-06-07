0a494df5a02fbf9dc7490253af087fc44840bc4e
Larry Taylor and Nicole Freed <larry@>
2017-06-07 14:44:33 -0700

    add backend logic to add way to end of words starting with vowels

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
 //business logic goes here //
 function pigLatin(english) {
-  return english;
+
+  // var vowels = ["a", "e", "i", "o", "u"]
+  // get char[0] of string
+  // iterate through vowels
+    // if char[0] === vowel
+    // newString = string + "way"
+
+  var vowels = ["a", "e", "i", "o", "u"];
+
+  for (index = 0; index <= vowels.length; index += 1){
+    if (english[0] === vowels[index]){
+      var newWord = english + "way";
+      return newWord;
+    };
+  };
 };
-
-
 
 // user interface logic goes here //
 $(function(){
